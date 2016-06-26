c8a3c79ea54ec4b6d22e079c038915a28e946b95
PloadyFree <PloadyFree@gmail.com>
2016-06-26 16:04:00 +0400

    Add ClassicGameFieldShuffler

---
+++ Puzzle15/Implementations/ClassicGameFieldShuffler.cs
@@ -0,0 +1,24 @@
+﻿using Puzzle15.Interfaces;
+using RectangularField.Core;
+
+namespace Puzzle15.Implementations
+{
+    public class ClassicGameFieldShuffler: IGameFieldShuffler<int>
+    {
+        public IRectangularField<int> Shuffle(IRectangularField<int> field, int quality)
+        {
+            var zeroLocation = field.GetLocation(0);
+
+            for (var i = 0; i < quality; i++)
+                foreach (var neighbour in zeroLocation.ByEdgeNeighbours.Shuffle())
+                    if (field.Contains(neighbour))
+                    {
+                        field = field.Swap(zeroLocation, neighbour);
+                        zeroLocation = neighbour;
+                        break;
+                    }
+
+            return field;
+        }
+    }
+}
---
+++ Puzzle15/Interfaces/IGameFieldShuffler.cs
@@ -0,0 +1,9 @@
+﻿using RectangularField.Core;
+
+namespace Puzzle15.Interfaces
+{
+    public interface IGameFieldShuffler<TCell>
+    {
+        IRectangularField<TCell> Shuffle(IRectangularField<TCell> field, int quality);
+    }
+}
